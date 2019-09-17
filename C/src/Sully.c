ed2feb9668373656f14c662ae4c094eefe9c693b
Samuel Jimenez <sjimenezre@gmail.com>
2019-09-18 00:35:03 +0200

    Fix String size C quine

--- C/src/Sully.c
+++ C/src/Sully.c
@@ -10,7 +10,7 @@ char path[20];
 sprintf(path, "Sully_%d.c", i);
 i--;
 f = open(path,O_CREAT|O_WRONLY|O_TRUNC, S_IRWXU);
-char src[500] = "#include <stdio.h>%1$c#include <fcntl.h>%1$c#include <unistd.h>%1$c#include <stdlib.h>%1$cint main(){%1$cint i = %2$d;%1$cif (i >= 0){%1$cint f;%1$cchar path[20];%1$csprintf(path, %3$cSully_%%d.c%3$c, i);%1$ci--;%1$cf = open(path,O_CREAT|O_WRONLY|O_TRUNC, S_IRWXU);%1$cchar src[5000] = %3$c%4$s%3$c;%1$cdprintf(f,src,10,i,34,src);%1$cchar cmd[100];%1$csprintf(cmd, %3$cgcc -Wall -Werror -Werror -o Sully_%%1$d Sully_%%1$d.c && ./Sully_%%1$d%3$c, i+1);%1$csystem(cmd);%1$cclose(f);%1$c}%1$c}%1$c";
+char src[500] = "#include <stdio.h>%1$c#include <fcntl.h>%1$c#include <unistd.h>%1$c#include <stdlib.h>%1$cint main(){%1$cint i = %2$d;%1$cif (i >= 0){%1$cint f;%1$cchar path[20];%1$csprintf(path, %3$cSully_%%d.c%3$c, i);%1$ci--;%1$cf = open(path,O_CREAT|O_WRONLY|O_TRUNC, S_IRWXU);%1$cchar src[500] = %3$c%4$s%3$c;%1$cdprintf(f,src,10,i,34,src);%1$cchar cmd[100];%1$csprintf(cmd, %3$cgcc -Wall -Werror -Werror -o Sully_%%1$d Sully_%%1$d.c && ./Sully_%%1$d%3$c, i+1);%1$csystem(cmd);%1$cclose(f);%1$c}%1$c}%1$c";
 dprintf(f,src,10,i,34,src);
 char cmd[100];
 sprintf(cmd, "gcc -Wall -Werror -Werror -o Sully_%1$d Sully_%1$d.c && ./Sully_%1$d", i+1);
